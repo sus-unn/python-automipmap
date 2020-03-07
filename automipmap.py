80026ad3cc7e8cbeb5d5e48efd0614b7165ee79d
ssn <psusunn@gmail.com>
2020-03-08 01:04:31 +0900

    bug fix

--- automipmap.py
+++ automipmap.py
@@ -1,15 +1,13 @@
 from PIL import Image
 
-src = Image.open("\source\source.png")
+src = Image.open("source\source.png")
 result = []
-count = range(1,4)
 resize = []
+count = 4
 
-for num in range:
-    result = "\results\img" + str(num) + ".png"
-    resize.append(src.resize((src.size[0]/(2*num), src.size[1]/(2*num))))
-    resize.save(result)
+for num in range(1, count):
+    result.append("results\img" + str(num) + ".png")
+    resize.append(src.resize((int(src.size[0]/(2*num)), int(src.size[1]/(2*num)))))
 
-
-
-
+for num in range(0, count-1):
+    resize[num].save(result[num])
